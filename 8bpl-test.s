b62bd46a0ba343e44a1276e4981dc3458213334f
Lauri Gratseff <lauri.smith@gmail.com>
2019-02-03 11:44:14 +0000

    startup codes initial (ham and 8bpl)

---
+++ 8bpl-test.s
@@ -0,0 +1,76 @@
+
+; test program for 8 bitplane startup code
+; compiles with phxass at least
+
+	MACHINE 68020
+
+	section code
+
+main
+	bsr init ; switch off system and set custom copperlist etc
+	bsr initC2P ; set parameters for chunky to planar routine
+
+	lea texture,a0
+	adda.l #6,a0 ; skip header in texture
+	move.l a0,newpalette
+	move.w #1,updatepaletteflag ; set flag to update palette in next vblank int
+
+.mainloop
+	
+		bsr drawTestScreen
+
+		lea screen,a0
+		bsr flipScreen
+
+		bsr waitVBlank
+		tst.w exitflag
+
+	beq .mainloop
+
+	bsr deinit
+
+	rts
+
+
+
+drawTestScreen
+	; draws a scrolling texture to chunky screen
+
+	lea screen,a0
+	lea texture,a1
+
+	move.l sync,d2 ; sync is our global timer incremented in vblank interrupt 50 times a second
+
+	move.l #176-1,d0
+.yloop
+
+	movea.l a1,a2
+	adda.l #6+256*4,a2 ; skip header and palette
+	adda.l d2,a2
+
+	move.l #(320/4/8)-1,d1
+.xloop
+		rept 8
+			move.l (a2)+,(a0)+
+		endr
+	dbra d1,.xloop
+
+	adda.l #256,a1
+
+	dbra d0,.yloop
+
+	rts
+
+
+	section data, bss
+
+screen
+	blk.b 320*176,0
+
+	section data
+
+texture
+	incbin "data/texture.uc"
+
+
+	include "8bpl-init.s"
---
+++ ham-test.s
@@ -0,0 +1,69 @@
+
+; test program for ham startup code
+; compiles with phxass at least
+
+	MACHINE 68020
+
+	section code
+
+main
+	bsr init ; switch off system and set custom copperlist etc
+	bsr initC2P ; set parameters for chunky to planar routine and setup bitplanes
+
+.mainloop
+	
+		bsr drawTestScreen
+
+		lea screen,a0
+		bsr flipScreen
+
+		bsr waitVBlank
+		tst.w exitflag
+
+	beq .mainloop
+
+	bsr deinit
+
+	rts
+
+
+
+drawTestScreen
+	; draws a scrolling texture to chunky screen
+
+	lea screen,a0
+	lea texture,a1
+
+	move.l sync,d2 ; sync is our global timer incremented in vblank interrupt 50 times a second
+	lsl.l #1,d2 ; shift up to scroll words
+
+	move.l #176-1,d0
+.yloop
+	movea.l a1,a2
+	adda.l d2,a2
+
+	move.l #(220/10)-1,d1
+.xloop
+		rept 10
+			move.w (a2)+,(a0)+
+			addq.l #2,a2 ; skip every other pixel
+		endr
+	dbra d1,.xloop
+
+	adda.l #(256<<1),a1
+	dbra d0,.yloop
+
+	rts
+
+
+	section data, bss
+
+screen
+	blk.w 220*176,0
+
+	section data
+
+texture
+	incbin "data/texture.c15"
+
+	include "ham-init.s"
